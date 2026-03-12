7b968a6c015fb20f97ee06ee2cec16e5dcefadf3
GiovannaOliveiraa <giovanna.o.santos7@aluno.senai.br>
2026-03-12 11:34:52 +0000

    Add files via upload

---
+++ Torta.java
@@ -0,0 +1,11 @@
+public class Torta extends Doce{
+
+    public static void cobertura(){
+    System.out.println("Merengue!");
+    }
+
+    public static void massa() {
+        System.out.println("Baunilha");
+    }
+}
+
---
+++ Vaca.java
@@ -0,0 +1,7 @@
+public class Vaca extends Animal{
+
+    public Vaca(String nome, String cor, double peso) {
+        super(nome, cor, peso);
+    }
+
+}
