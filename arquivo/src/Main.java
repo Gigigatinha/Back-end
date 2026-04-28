282ab711a6f1c0ae2ded0540a2de9958ceb105bc
GiovannaOliveiraa <giovanna.o.santos7@aluno.senai.br>
2026-04-28 09:04:22 -0300

    Add files via upload

---
+++ arquivo/src/Main.java
@@ -0,0 +1,11 @@
+import java.nio.file.Path;
+
+void main() throws IOException {
+            ContaBancaria conta = new ContaBancaria(100);
+
+            try {
+                conta.sacar(150);
+            } catch (SaldoInsuficienteException e) {
+                System.out.println(e.getMessage());
+            }
+}
